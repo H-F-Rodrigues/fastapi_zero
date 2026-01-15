172484869119ccf4f080aa81cc6ac7bc24e516c3
Henrique Ferreira Rodrigues <henriquefrodrigues1225@gmail.com>
2026-01-15 18:13:13 -0300

    AULA 02

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='1ª API')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/HW', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+      <head>
+        <title> Meu Olá Mundo!</title>
+      </head>
+      <body>
+        <h1> Hello World! </h1>
+        <h1> Te amo Jhennyfer!!! <3</h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
+
 
 def test_root_deve_retornar_ola_mundo():
     """
@@ -13,4 +16,26 @@ def test_root_deve_retornar_ola_mundo():
 
     response = client.get('/')
 
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_hello_world_deve_retornar_html():
+    client = TestClient(app)
+
+    response = client.get('/HW')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Meu Olá Mundo!</title>
+      </head>
+      <body>
+        <h1> Hello World! </h1>
+        <h1> Te amo Jhennyfer!!! <3</h1>
+      </body>
+    </html>"""
+    )
